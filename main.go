9ee4bf1c00070c4712ebaf2efe21075063a0cc64
Fareed Dudhia <fareeddudhia@googlemail.com>
2015-09-29 23:05:41 +0100

    Refactor layers

--- main.go
+++ main.go
@@ -9,28 +9,48 @@ func main() {
     gogol.Init(gogol.WindowOpts{Title: "Gogol Example", Width: 640, Height: 480})
     defer gogol.Cleanup()
 
-    gogol.OnKeyDown(gogol.K_RETURN, hideStuff)
-    gogol.OnKeyUp(gogol.K_RETURN, showStuff)
+    gogol.OnKeyDown(gogol.K_Q, hideLayer("inner"))
+    gogol.OnKeyUp(gogol.K_Q, showLayer("inner"))
+
+    gogol.OnKeyDown(gogol.K_W, hideLayer("outer"))
+    gogol.OnKeyUp(gogol.K_W, showLayer("outer"))
+
     gogol.OnMouseDown(gogol.M_LEFT, doMouseStuff)
 
-    p := gogol.NewQuad(gogol.QuadOpts{Width: 25.0, Height: 25.0})
-    g := gogol.NewQuad(gogol.QuadOpts{Width: 25.0, Height: 25.0})
+    q1 := gogol.NewQuad(gogol.QuadOpts{Width: 25.0, Height: 25.0})
+    q2 := gogol.NewQuad(gogol.QuadOpts{Width: 25.0, Height: 25.0})
+    q3 := gogol.NewQuad(gogol.QuadOpts{Width: 25.0, Height: 25.0})
 
-    g.Translate(100.0, 0.0)
-    p.AddChild(g)
+    q1.AddChild(q2)
+    q2.AddChild(q3)
+
+    q2.Translate(100.0, 0.0)
+    q3.Translate(50.0, 0.0)
+
+    gogol.GetLayer("base").AppendLayer("inner")
+    gogol.GetLayer("inner").AppendLayer("outer")
+
+    q2.AddToLayer("inner")
+    q3.AddToLayer("outer")
 
     for !gogol.ShouldQuit() {
         gogol.ProcessOneFrame()
-        p.Rotate(0.1)
+        q1.Rotate(0.1)
+        q2.Rotate(0.1)
+        q3.Rotate(0.1)
     }
 }
 
-func hideStuff(code gogol.KeyCode) {
-    gogol.GetLayers("default").Hide()
+func hideLayer(name string) (func(code gogol.KeyCode)) {
+    return func(code gogol.KeyCode) {
+        gogol.GetLayer(name).Hide()
+    }
 }
 
-func showStuff(code gogol.KeyCode) {
-    gogol.GetLayers("default").Show()
+func showLayer(name string) (func(code gogol.KeyCode)) {
+    return func(code gogol.KeyCode) {
+        gogol.GetLayer(name).Show()
+    }
 }
 
 func doMouseStuff(code gogol.KeyCode) {
